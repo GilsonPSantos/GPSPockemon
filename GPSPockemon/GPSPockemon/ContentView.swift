4f5ebc96af109e5482573c86b5f28284d7550611
Gilson Santos <gilsonsantos@gmail.com>
2020-03-29 20:44:09 -0300

    Criacao do projeto

---
+++ GPSPockemon/GPSPockemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPSPockemon
+//
+//  Created by Gilson Santos on 29/03/20.
+//  Copyright © 2020 Gilson Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
